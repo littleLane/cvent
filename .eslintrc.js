7094d48ed35c4e38ac1d50d8813c63741dee1654
littleLane <lzz857183384@163.com>
2020-04-03 23:34:49 +0800

    feat: init project

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // 解析器
+  parser: '@typescript-eslint/parser',
+
+  // 继承的规则 [扩展]
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+
+  // 插件
+  plugins: ['@typescript-eslint'],
+
+  // 规则
+  rules: {
+
+  }
+}
