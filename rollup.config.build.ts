7f0b3c8a31592888d379f2bb5df7d893e0b6eceb
littleLane <lzz857183384@163.com>
2020-04-05 19:15:57 +0800

    feat: optimize packager config

---
+++ rollup.config.build.ts
@@ -0,0 +1,73 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+import typescript from 'rollup-plugin-typescript2'
+import { eslint } from 'rollup-plugin-eslint'
+import { terser } from 'rollup-plugin-terser'
+
+const pkg = require('./package.json')
+
+const libraryName = 'Cvent'
+const banner =
+  `//  Cvent v${pkg.version}\n` +
+  '//  https://github.com/littleLane/cvent\n' +
+  `//  (c) 2020-${new Date().getFullYear()} littleLane\n` +
+  '//  Cvent may be freely distributed under the MIT license.\n'
+
+const basicPlugins = [
+  // Allow json resolution
+  eslint(),
+  // Compile TypeScript files
+  typescript({ useTsconfigDeclarationDir: true }),
+  // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+  commonjs(),
+  // Allow node_modules resolution, so you can use 'external' to control
+  // which external modules to include in the bundle
+  // https://github.com/rollup/rollup-plugin-node-resolve#usage
+  resolve(),
+
+  // Resolve source maps to the original source
+  sourceMaps(),
+]
+
+export default [
+  {
+    input: `src/${libraryName}.ts`,
+    output: {
+      file: pkg.main,
+      format: 'umd',
+      name: libraryName,
+      banner: banner,
+    },
+    plugins: basicPlugins,
+  },
+  {
+    input: `src/${libraryName}.ts`,
+    output: {
+      file: pkg.unpkg,
+      format: 'umd',
+      name: libraryName,
+      banner: banner,
+    },
+    plugins: basicPlugins.concat([
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false,
+        },
+      }),
+    ]),
+  },
+  {
+    input: `src/${libraryName}.ts`,
+    output: {
+      file: pkg.module,
+      format: 'es',
+      name: libraryName,
+      banner: banner,
+    },
+    plugins: basicPlugins,
+  },
+]
--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import json from '@rollup/plugin-json'
-import sourceMaps from 'rollup-plugin-sourcemaps'
 import typescript from 'rollup-plugin-typescript2'
 import { eslint } from 'rollup-plugin-eslint'
 
@@ -12,17 +10,16 @@ const libraryName = 'cvent'
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: libraryName, format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    {
+      file: pkg.main,
+      name: libraryName,
+      format: 'umd',
+    },
   ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
   watch: {
     include: 'src/**',
   },
   plugins: [
-    // Allow json resolution
-    json(),
     eslint(),
     // Compile TypeScript files
     typescript({ useTsconfigDeclarationDir: true }),
@@ -32,8 +29,5 @@ export default {
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
-
-    // Resolve source maps to the original source
-    sourceMaps(),
   ],
 }
