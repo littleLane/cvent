7588a7d4cd45a5a9555911f5cc2346c9ecb16387
littleLane <lzz857183384@163.com>
2020-04-05 19:29:10 +0800

    feat: optimize eslint and rollup config

--- rollup.config.build.ts
+++ rollup.config.build.ts
@@ -1,6 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
 import typescript from 'rollup-plugin-typescript2'
 import { eslint } from 'rollup-plugin-eslint'
 import { terser } from 'rollup-plugin-terser'
@@ -25,9 +24,6 @@ const basicPlugins = [
   // which external modules to include in the bundle
   // https://github.com/rollup/rollup-plugin-node-resolve#usage
   resolve(),
-
-  // Resolve source maps to the original source
-  sourceMaps(),
 ]
 
 export default [
