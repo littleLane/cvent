71a96b01908a15751525d3651ce5afe529cbe720
littleLane <lzz857183384@163.com>
2020-04-16 22:30:43 +0800

    feat: add node example

---
+++ examples/node/index.ts
@@ -0,0 +1,11 @@
+const Cvent = require('cvent')
+
+const cvent = new Cvent()
+
+cvent.on('click', (data) => {
+  console.log(data)
+})
+
+cvent.emit('click', { name: 'cvent1' })
+cvent.off('click')
+cvent.emit('click', { name: 'cvent2' })
